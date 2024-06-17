030ea50e057144296182f663d467640d6ab2cb49
Gaetan-bamongoyo <g.a.bamongoyo@gmail.com>
2024-06-17 14:15:41 +0200

    loading

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -2,7 +2,12 @@ import Sidebar from "../ui/dashboard/sidebar/sidebar"
 import Navbar from "../ui/dashboard/navbar/navbar"
 import styles from "../ui/dashboard/dashboard.module.css"
 import Footer from "../ui/dashboard/footer/footer"
-const Layout = ({children}) => {
+import React, { ReactNode } from "react";
+interface Props {
+  children?: ReactNode
+  // any props that come into the component
+}
+const Layout = ({children, ...props}: Props) => {
     return (
       <div className={styles.container}>
         <div className={styles.menu}>
